026895def3e7bb3b968fc53d306a48f6f2ceaa29
JungHwan Park <pjhool0612@gmail.com>
2025-02-10 00:10:21 +0900

    Add files via upload

---
+++ config.py
@@ -0,0 +1,9 @@
+import os
+
+OPENAI_API_KEY="zzz"
+
+def set_environment():
+    variable_dict = globals().items()
+    for key, value in variable_dict :
+        if "API" in key or "ID" in key :
+            os.environ[key] = value
